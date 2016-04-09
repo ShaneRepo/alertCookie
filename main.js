6d4352cde3babcfabee9e5baef22da1d5698c11c
Shane <shane8641@live.com>
2016-04-09 12:43:10 -0400

    setting cookies from form and displaying with alert

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+function submitValues(){
+    var firstname = document.forms["frm1"]["firstname"].value;
+    var lastname = document.forms["frm1"]["lastname"].value;
+    var phone = document.forms["frm1"]["phone"].value;
+    var address = document.forms["frm1"]["address"].value;
+    var car = document.forms["frm1"]["car"].value;
+    document.cookie = "firstname=" + firstname;
+    document.cookie = "lastname=" + lastname;
+    document.cookie = "phone=" + phone;
+    document.cookie = "address=" + address;
+    document.cookie = "address=" + car;
+    return alert(document.cookie);
+}
